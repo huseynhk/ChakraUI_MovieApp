3160de4f40bba50aea6f3ad494370731349d7973
setap <khuseyn693@gmail.com>
2024-04-07 17:24:44 +0400

    loading_Cards_ActiveLink

--- movie/src/components/Cards.jsx
+++ movie/src/components/Cards.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { Image, Box, Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { imagePath } from "../services/api";
+import { StarIcon } from "@chakra-ui/icons";
+import { ROUTER } from "../constant/router";
 
-const Cards = () => {
+const Cards = ({ item, type }) => {
   return (
-    <div>Cards</div>
-  )
-}
+    <Link>
+      <Box
+        position={"relative"}
+        transform={"scale(1)"}
+        _hover={{
+          transform: { base: "scale(1)x", md: "scale(1.08)" },
+          transition: "transform .5s ease-in-out",
+          zIndex: "10",
+          "& .overlay": {
+            opacity: 1,
+          },
+        }}
+      >
+        <Image
+          src={`${imagePath}/${item?.poster_path}`}
+          alt={item?.title || item?.name}
+          height={"100%"}
+        />
+        <Box
+          className="overlay"
+          pos={"absolute"}
+          pb="2"
+          bottom={"0"}
+          left={"0"}
+          w={"100%"}
+          h={"25%"}
+          bg="rgba(0,0,0,0.9)"
+          opacity={"0"}
+          transition={"opacity .7s ease-in-out"}
+        >
+          <Text textAlign={"center"} mt="1" fontSize='20px' color={"blue.200"} >{item?.title || item?.name}</Text>
+          <Text textAlign={"center"} my="2" fontSize='xl' color={"green.200"}>
+            {new Date(
+              item?.release_date || item?.first_air_date
+            ).getFullYear() || "N/A"}
+          </Text>
+          <Flex alignItems={"center"} justifyContent={"center"} gap={2} mb="2" color={"cyan.200"}>
+            <StarIcon fontSize={"medium"} />
+            <Text>{item?.vote_average?.toFixed(1)}</Text>
+          </Flex>
+        </Box>
+      </Box>
+    </Link>
+  );
+};
 
-export default Cards
+export default Cards;
--- movie/src/pages/Home.jsx
+++ movie/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Container, Heading, Grid, Image } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  Grid,
+  Flex,
+  Box,
+  Skeleton,
+} from "@chakra-ui/react";
 import { fetchTrending } from "../services/api";
-import { imagePath } from "../services/api";
 import Cards from "../components/Cards";
 
 const Home = () => {
@@ -23,26 +29,59 @@ const Home = () => {
       });
   }, [timeWindow]);
 
-  console.log(data, "data");
-
   return (
     <Container maxW={"container.xl"}>
-      <Heading as={"h2"} fontSize={"md"} textTransform={"uppercase"}>
-        Trending
-      </Heading>
+      <Flex alignItems={"baseline"} my={"7"} gap={"5"}>
+        <Heading as={"h2"} fontSize={"md"} textTransform={"uppercase"}>
+          Trending
+        </Heading>
+
+        <Flex
+          alignItems={"center"}
+          gap={"5"}
+          border={"2px solid teal"}
+          borderRadius={"20px"}
+        >
+          <Box
+            as="button"
+            borderRadius={"20px"}
+            py="1"
+            px="3"
+            bg={`${timeWindow === "day" ? "teal.700" : ""}`}
+            onClick={() => setTimeWindow("day")}
+          >
+            Today
+          </Box>
+          <Box
+            as="button"
+            borderRadius={"20px"}
+            py="1"
+            px="3"
+            bg={`${timeWindow === "week" ? "teal.700" : ""}`}
+            onClick={() => setTimeWindow("week")}
+          >
+            This Week
+          </Box>
+        </Flex>
+      </Flex>
 
       <Grid
         templateColumns={{
-          sm: "1fr",
-          md: "repeat(2, 1fr)",
+          base: "1fr",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(3, 1fr)",
           lg: "repeat(4, 1fr)",
         }}
-        gap={"6"}
+        gap={"5"}
       >
         {data &&
-          data.map((item) => (
-            <Image key={item.id} src={`${imagePath}/${item.poster_path}`} />
-          ))}
+          data?.map((item, index) =>
+            loading ? (
+              <Skeleton height={300} key={index} />
+            ) : (
+              <Cards key={item?.id} item={item} type={item?.media_type} />
+            )
+          )}
       </Grid>
     </Container>
   );
